b5de0536f30ec71c2431c610f50fff8a1b754c39
Affan <techmaster3424@gmail.com>
2024-08-16 18:44:27 +0530

    for laptop

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -3,8 +3,6 @@ import { getProduct, loadProductsFetch } from "../data/products.js";
 import { getOrder, orders } from "../data/orders.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 
-
-
 async function loadPage() {
   updateHeaderCartQuantity();
   await loadProductsFetch();
@@ -13,19 +11,24 @@ async function loadPage() {
 
   const orderId = url.searchParams.get("orderId");
   const product = getProduct(productId);
-  const order = getOrder(orderId)
+  const order = getOrder(orderId);
   let matchingOrderProduct;
-  
- console.log(orders)
-      order.products.forEach((product) => {
-        if ((product.productId = productId)) {
-          matchingOrderProduct = product;
-        }
-      });
+
+  console.log(orders);
+  order.products.forEach((product) => {
+    if ((product.productId = productId)) {
+      matchingOrderProduct = product;
+    }
+  });
   let deliveryTime = dayjs(matchingOrderProduct.estimatedDeliveryTime).format(
     "dddd MMMM D"
   );
-
+  const today = dayjs();
+  const orderTime = dayjs(order.orderTime);
+  const deliveryTimeOrder = dayjs(matchingOrderProduct.estimatedDeliveryTime);
+  const percentProgress =
+    ((today - orderTime) / (deliveryTimeOrder - orderTime)) * 100;
+  console.log(percentProgress);
   let trackingHtml = ` 
          <a class="back-to-orders-link link-primary" href="orders.html">
           View all orders
@@ -37,7 +40,9 @@ async function loadPage() {
           ${product.name}
         </div>
 
-        <div class="product-info">Quantity: ${matchingOrderProduct.quantity}</div>
+        <div class="product-info">Quantity: ${
+          matchingOrderProduct.quantity
+        }</div>
 
         <img
           class="product-image"
@@ -45,15 +50,25 @@ async function loadPage() {
         />
 
         <div class="progress-labels-container">
-          <div class="progress-label">Preparing</div>
-          <div class="progress-label current-status">Shipped</div>
-          <div class="progress-label">Delivered</div>
+          <div class="progress-label
+          ${percentProgress < 50 ? "current-status" : ""}
+          ">Preparing</div>
+          <div class="progress-label 
+          ${percentProgress >= 50 ? "current-status" : ""}
+          ">Shipped</div>
+          <div class="progress-label
+          ${percentProgress >= (100 ? "current-status" : "")}
+          ">Delivered</div>
         </div>
 
         <div class="progress-bar-container">
-          <div class="progress-bar"></div>
+          <div style="width:${percentProgress}%;" class="progress-bar"></div>
         </div>
         `;
   document.querySelector(".js-order-tracking").innerHTML = trackingHtml;
 }
 loadPage();
+document.querySelector(".search-button").addEventListener("click", () => {
+  let keyword = document.querySelector(".search-bar").value;
+  window.location.href = `amazon.html?search=${keyword}`;
+});
